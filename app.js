7934a008825b386dbc3c63c821a0d75a5b1120e8
saiiiyan <kunalaggarwal58@gmail.com>
2019-03-15 00:34:07 +0530

    Exporting entities

---
+++ app.js
@@ -0,0 +1,5 @@
+const tutorial = require('./lib');
+console.log(tutorial);
+console.log(tutorial.sum(12,44));
+console.log(tutorial.PI);
+console.log(new tutorial.Hello());
---
+++ lib.js
@@ -0,0 +1,8 @@
+const sum = (num1,num2) => num1+num2;
+const PI  = 3.14;
+class Hello{
+    constructor(){
+        console.log('Hello Class');
+    }
+}
+module.exports = {sum:sum,PI:PI,Hello:Hello};
