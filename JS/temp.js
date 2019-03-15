7aad23b0a36bc2cab50899641afc92159d94fd3a
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-03-15 11:28:58 +0000

    Revising basic concepts

---
+++ JS/temp.js
@@ -0,0 +1,29 @@
+var x='1';
+var y;
+var myArr=[1,'2',1.2,2/0,function(){},null,y+1];
+
+// console.log("firstLine\n\tSecondLine\n\t\bThirdline");
+// console.log(typeof("2"+1));
+// console.log(typeof(1+"2"));
+
+// for(let i=0;i<myArr.length;i++){
+//     console.log(typeof(myArr[i]));
+// }
+// for(let i=0;i<myArr.length;i++){
+//     console.log(myArr[i]);
+// }
+
+// if(typeof y!='undefined')console.log('YES');
+// console.log(myArr[6]);
+//console.log(1!=='1');
+
+// var car1 = {
+//  "color": "red"    
+// };
+// var car2 = car1;
+// console.log(car1.color);
+// car1.color = "blue";
+// console.log(car1.color);
+// console.log(car2.color);
+
+console.log(parseInt('1010',10));
